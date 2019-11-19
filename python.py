8119fb3c5ea809088cbf2d76c247e825717c2d3f
Taha <ahma00d@gmial.com>
2019-11-19 06:47:29 -0800

    first commit

---
+++ python.py
@@ -0,0 +1,189 @@
+
+"""Q1"""
+print("Hello")
+print("\tHello")
+print("\t\tHello")
+
+
+
+"""Q2"""
+print(" Orange Academy "*20)
+
+
+
+
+"""Q3"""
+x=float(1)
+y=float(2.8)
+z=float('3')
+w=float('4.2')
+
+print(x,y,z,w,sep='10')
+"""   1.0102.8103.0104.2 """
+print(x,y,z,w,sep='---')
+"""   1.0---2.8---3.0---4.2   """
+
+
+"""Q4"""
+a=int(input('Press First  Number'))
+b=int(input('Press Second Number'))
+print(a*b)
+
+
+
+
+"""Q5"""
+a=int(input('Enter First  Number'))
+b=int(input('Enter Second Number'))
+c=int(input('Enter Third  Number'))
+d=int(input('Enter Forth  Number'))
+e=int(input('Enter Fifth  Number'))
+print((a+b+c+d+e)/5)
+
+
+
+
+
+"""Q6"""
+
+""" 
+The Questions
+x=1
+y=2.8
+z=1j
+o="Orange"
+A=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+
+
+
+Solution
+
+
+1)1     =>  is number
+2)2.8        =>  is float
+3)1j       => is complex
+4)Orange   => is string
+5)True  => is boolean   
+
+
+"""
+
+
+
+
+
+
+"""Q7"""
+
+""" 
+The Questions
+x,y="Orange",1
+X1,Y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(Y1)
+
+
+Solution
+
+1)Orange
+2)1
+3)name 'x1' is not defined
+4)-10
+
+"""
+
+
+
+
+
+
+
+"""Q8"""
+"""
+1-variables must not start with number
+2-variables must not have Speachal charcater
+3-1 variable must not have space
+4-Print wrote in python in small case
+
+"""
+ 
+
+
+"""Q9"""
+""" is repeated question """
+
+
+
+
+"""Q10"""
+""" is repeated question """
+
+
+
+
+"""Q11"""
+"""   
+#print("Hello,World!")    =>its comment
+print("Cheers,mate!")     =>Cheers,mate!
+
+Testing The Code => its comment
+    
+"""  
+
+
+"""Q12"""
+a=(input('Enter Your  Name'))
+b=int(input('What is your age  '))
+c=(100 - b) + 2019
+print("You Will Be 100 Years old in : " + str(c)  )
+
+
+
+
+
+
+
+
+
+
+"""Q13"""
+base=int(input('Enter First  Number'))
+height=int(input('Enter Second Number'))
+print((base*height)/2)
+
+
+
+
+
+
+
+
+
+
+
+
+"""Q14"""
+
+"""
+20
+8
+33
+3.66666
+3
+2
+11
+"11"
+11.0
+print(divmod(11,3))  =>   (3,2)
+print(pow(11,3))     =>   1331
+true
+false
+false
+"""
